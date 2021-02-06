bbad80346c648522988ea162eebe5c3792ff1946
Matt Wiener <mwiener@outlook.com>
2021-02-06 11:02:31 -0500

    revert breaking change to ConnectWiseModel
    Signed-off-by: Matt Wiener <mwiener@outlook.com>

--- Models/ConnectWiseModel.cs
+++ Models/ConnectWiseModel.cs
@@ -4,13 +4,13 @@ namespace ThinkVoipTool.Models
 {
     public class ConnectWiseModel
     {
-        protected string? CompanyName { get; set; }
-        protected string? Codebase { get; set; }
+        public string? CompanyName { get; set; }
+        public string? Codebase { get; set; }
         public string? VersionCode { get; set; }
         public string? VersionNumber { get; set; }
         public string? CompanyId { get; set; }
         public bool IsCloud { get; set; }
-        protected string? SiteUrl { get; set; }
+        public string? SiteUrl { get; set; }
         public List<string>? AllowedOrigins { get; set; }
 
         public static ConnectWiseModel CreateInstance() => new ConnectWiseModel();
